5e455ed6e43bab3796e6d65c755fc01e20c66045
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-10-19 11:41:57 +0300

    +add: load model

--- src/models/load.model.js
+++ src/models/load.model.js
@@ -1,27 +1,77 @@
 const { Schema, model, SchemaTypes } = require('mongoose')
 
-const truck = new Schema(
+const load = new Schema(
   {
-    userId: {
+    created_by: {
       type: SchemaTypes.String,
-      required: [true, 'userId is required']
+      required: [true, 'created_by is required']
     },
-    text: {
+    assigned_to: {
       type: SchemaTypes.String,
-      required: [true, 'Text is required']
+      default: null
     },
-    createdDate:
-    {
+    status: {
+      type: SchemaTypes.String,
+      derequired: [true, 'Status is required']
+    },
+    state: {
+      type: SchemaTypes.String,
+      default: ''
+    },
+    name: {
+      type: SchemaTypes.String,
+      required: [true, 'Name is required']
+    },
+    payload: {
+      type: SchemaTypes.Number,
+      required: [true, 'Payload is required']
+    },
+    pickup_address: {
+      type: SchemaTypes.String,
+      required: [true, 'Pickup address is required']
+    },
+      delivery_address: {
+      type: SchemaTypes.String,
+      required: [true, 'Delivery address is required']
+    },
+    dimensions: {
+        type: SchemaTypes.Map,
+        of: new Schema({
+          width: {
+            type: SchemaTypes.Number,
+            required: [true, 'Width is required']
+          },
+          length: {
+            type: SchemaTypes.Number,
+            required: [true, 'Length is required']
+          },
+          height: {
+            type: SchemaTypes.Number,
+            required: [true, 'Height is required']
+          }
+        })
+    },
+    logs: {
+      type: SchemaTypes.Array,
+      of: {
+        message: {
+          type: SchemaTypes.String,
+          required: [true, 'Log message is required']
+        },
+        time: {
+          type: SchemaTypes.String,
+          required: [true, 'Message time is required']
+        }
+      },
+      default: []
+    },
+    created_date: {
       type: SchemaTypes.String,
       required: [true, 'Creation date is required']
-    },
-    completed: {
-      type: SchemaTypes.Boolean,
-      default: false
     }
   }
 )
 
-const Truck = model('trucks', truck)
+const Load = model('loads', load)
 
-module.exports = Truck
+module.exports = Load
