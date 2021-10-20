77c27fe2bc54574886d30b781a84269ca0759d90
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-10-20 13:38:00 +0300

    ~fix: pasword resed via sendgrid

--- src/helpers/mailServise.js
+++ src/helpers/mailServise.js
@@ -1,10 +1,5 @@
-// const nodemailer = require('nodemailer')
-// const { MAIL_SETTINGS } = require('../configs/mail.config')
-
-// const mailServise = nodemailer.createTransport(MAIL_SETTINGS);
-
-const mailServise = require('@sendgrid/mail')
 require('dotenv').config()
+const mailServise = require('@sendgrid/mail')
 mailServise.setApiKey(process.env.SENDGRID_API_KEY)
 
 module.exports = {
